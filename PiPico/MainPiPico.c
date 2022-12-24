836e61b100a502a99c1d0514a72913502677aab1
Cesare Giavardi <GiavarCe@users.noreply.github.com>
2022-12-24 15:48:13 +0100

    Added SSD1306 oled display
    Display: 0.91 inch, 128x32 pixels.

--- PiPico/MainPiPico.c
+++ PiPico/MainPiPico.c
@@ -1,4 +1,11 @@
 /**
+ * GPIO PICO_DEFAULT_I2C_SDA_PIN (on Pico this is GP4 (pin 6)) -> SDA on display
+   board
+   GPIO PICO_DEFAULT_I2C_SCK_PIN (on Pico this is GP5 (pin 7)) -> SCL on
+   display board
+   3.3v (pin 36) -> VCC on display board
+   GND (pin 38)  -> GND on display board
+
  * Xbee PRO S1 configured as API 1
  * Baud rate = 19200 b/s
  * 
@@ -6,15 +13,56 @@
 #include "Globals.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
 #include "hardware/i2c.h"
 #include "pico/binary_info.h"
+#include "raspberry26x32.h"
 
+// commands (see datasheet)
+#define OLED_SET_CONTRAST _u(0x81)
+#define OLED_SET_ENTIRE_ON _u(0xA4)
+#define OLED_SET_NORM_INV _u(0xA6)
+#define OLED_SET_DISP _u(0xAE)
+#define OLED_SET_MEM_ADDR _u(0x20)
+#define OLED_SET_COL_ADDR _u(0x21)
+#define OLED_SET_PAGE_ADDR _u(0x22)
+#define OLED_SET_DISP_START_LINE _u(0x40)
+#define OLED_SET_SEG_REMAP _u(0xA0)
+#define OLED_SET_MUX_RATIO _u(0xA8)
+#define OLED_SET_COM_OUT_DIR _u(0xC0)
+#define OLED_SET_DISP_OFFSET _u(0xD3)
+#define OLED_SET_COM_PIN_CFG _u(0xDA)
+#define OLED_SET_DISP_CLK_DIV _u(0xD5)
+#define OLED_SET_PRECHARGE _u(0xD9)
+#define OLED_SET_VCOM_DESEL _u(0xDB)
+#define OLED_SET_CHARGE_PUMP _u(0x8D)
+#define OLED_SET_HORIZ_SCROLL _u(0x26)
+#define OLED_SET_SCROLL _u(0x2E)
+
+#define OLED_ADDR _u(0x3C)
+#define OLED_HEIGHT _u(32)
+#define OLED_WIDTH _u(128)
+#define OLED_PAGE_HEIGHT _u(8)
+#define OLED_NUM_PAGES OLED_HEIGHT / OLED_PAGE_HEIGHT
+#define OLED_BUF_LEN (OLED_NUM_PAGES * OLED_WIDTH)
+
+#define OLED_WRITE_MODE _u(0xFE)
+#define OLED_READ_MODE _u(0xFF)
 
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 const uint GP16_PIN = 16; //pin 21
 const uint GP28_PIN = 28; //pin 34
+
+struct render_area {
+    uint8_t start_col;
+    uint8_t end_col;
+    uint8_t start_page;
+    uint8_t end_page;
+
+    int buflen;
+};
 
 struct funct_out {
 	bool	Done;
@@ -35,6 +83,58 @@ struct structSystemTimer {
 	bool g_os_1m;
 	absolute_time_t g_now;
 } g_systemTimers;
+
+void fill(uint8_t buf[], uint8_t fill) {
+    // fill entire buffer with the same byte
+    for (int i = 0; i < OLED_BUF_LEN; i++) {
+        buf[i] = fill;
+    }
+};
+
+void fill_page(uint8_t *buf, uint8_t fill, uint8_t page) {
+    // fill entire page with the same byte
+    memset(buf + (page * OLED_WIDTH), fill, OLED_WIDTH);
+};
+
+// convenience methods for printing out a buffer to be rendered
+// mostly useful for debugging images, patterns, etc
+
+void print_buf_page(uint8_t buf[], uint8_t page) {
+    // prints one page of a full length (128x4) buffer
+    for (int j = 0; j < OLED_PAGE_HEIGHT; j++) {
+        for (int k = 0; k < OLED_WIDTH; k++) {
+            printf("%u", (buf[page * OLED_WIDTH + k] >> j) & 0x01);
+        }
+        printf("\n");
+    }
+}
+
+void print_buf_pages(uint8_t buf[]) {
+    // prints all pages of a full length buffer
+    for (int i = 0; i < OLED_NUM_PAGES; i++) {
+        printf("--page %d--\n", i);
+        print_buf_page(buf, i);
+    }
+}
+
+void print_buf_area(uint8_t *buf, struct render_area *area) {
+    // print a render area of generic size
+    int area_width = area->end_col - area->start_col + 1;
+    int area_height = area->end_page - area->start_page + 1; // in pages, not pixels
+    for (int i = 0; i < area_height; i++) {
+        for (int j = 0; j < OLED_PAGE_HEIGHT; j++) {
+            for (int k = 0; k < area_width; k++) {
+                printf("%u", (buf[i * area_width + k] >> j) & 0x01);
+            }
+            printf("\n");
+        }
+    }
+}
+
+void calc_render_area_buflen(struct render_area *area) {
+    // calculate how long the flattened buffer will be for a render area
+    area->buflen = (area->end_col - area->start_col + 1) * (area->end_page - area->start_page + 1);
+}
 
 void SYSTEM_Clock() {
 static bool firstScan=0, pr_50ms=false, pr_500ms=false, pr_30s=false;
@@ -221,6 +321,109 @@ struct bmp280_calib_param {
 };
 
 #ifdef i2c_default
+
+void oled_send_cmd(uint8_t cmd) {
+    // I2C write process expects a control byte followed by data
+    // this "data" can be a command or data to follow up a command
+
+    // Co = 1, D/C = 0 => the driver expects a command
+    uint8_t buf[2] = {0x80, cmd};
+    i2c_write_blocking(i2c_default, (OLED_ADDR & OLED_WRITE_MODE), buf, 2, false);
+}
+
+void oled_send_buf(uint8_t buf[], int buflen) {
+    // in horizontal addressing mode, the column address pointer auto-increments
+    // and then wraps around to the next page, so we can send the entire frame
+    // buffer in one gooooooo!
+
+    // copy our frame buffer into a new buffer because we need to add the control byte
+    // to the beginning
+
+    // TODO find a more memory-efficient way to do this..
+    // maybe break the data transfer into pages?
+    uint8_t *temp_buf = malloc(buflen + 1);
+
+    for (int i = 1; i < buflen + 1; i++) {
+        temp_buf[i] = buf[i - 1];
+    }
+    // Co = 0, D/C = 1 => the driver expects data to be written to RAM
+    temp_buf[0] = 0x40;
+    i2c_write_blocking(i2c_default, (OLED_ADDR & OLED_WRITE_MODE), temp_buf, buflen + 1, false);
+
+    free(temp_buf);
+}
+
+void oled_init() {
+    // some of these commands are not strictly necessary as the reset
+    // process defaults to some of these but they are shown here
+    // to demonstrate what the initialization sequence looks like
+
+    // some configuration values are recommended by the board manufacturer
+
+    oled_send_cmd(OLED_SET_DISP | 0x00); // set display off
+
+    /* memory mapping */
+    oled_send_cmd(OLED_SET_MEM_ADDR); // set memory address mode
+    oled_send_cmd(0x00); // horizontal addressing mode
+
+    /* resolution and layout */
+    oled_send_cmd(OLED_SET_DISP_START_LINE); // set display start line to 0
+
+    oled_send_cmd(OLED_SET_SEG_REMAP | 0x01); // set segment re-map
+    // column address 127 is mapped to SEG0
+
+    oled_send_cmd(OLED_SET_MUX_RATIO); // set multiplex ratio
+    oled_send_cmd(OLED_HEIGHT - 1); // our display is only 32 pixels high
+
+    oled_send_cmd(OLED_SET_COM_OUT_DIR | 0x08); // set COM (common) output scan direction
+    // scan from bottom up, COM[N-1] to COM0
+
+    oled_send_cmd(OLED_SET_DISP_OFFSET); // set display offset
+    oled_send_cmd(0x00); // no offset
+
+    oled_send_cmd(OLED_SET_COM_PIN_CFG); // set COM (common) pins hardware configuration
+    oled_send_cmd(0x02); // manufacturer magic number
+
+    /* timing and driving scheme */
+    oled_send_cmd(OLED_SET_DISP_CLK_DIV); // set display clock divide ratio
+    oled_send_cmd(0x80); // div ratio of 1, standard freq
+
+    oled_send_cmd(OLED_SET_PRECHARGE); // set pre-charge period
+    oled_send_cmd(0xF1); // Vcc internally generated on our board
+
+    oled_send_cmd(OLED_SET_VCOM_DESEL); // set VCOMH deselect level
+    oled_send_cmd(0x30); // 0.83xVcc
+
+    /* display */
+    oled_send_cmd(OLED_SET_CONTRAST); // set contrast control
+    oled_send_cmd(0xFF);
+
+    oled_send_cmd(OLED_SET_ENTIRE_ON); // set entire display on to follow RAM content
+
+    oled_send_cmd(OLED_SET_NORM_INV); // set normal (not inverted) display
+
+    oled_send_cmd(OLED_SET_CHARGE_PUMP); // set charge pump
+    oled_send_cmd(0x14); // Vcc internally generated on our board
+
+    oled_send_cmd(OLED_SET_SCROLL | 0x00); // deactivate horizontal scrolling if set
+    // this is necessary as memory writes will corrupt if scrolling was enabled
+
+    oled_send_cmd(OLED_SET_DISP | 0x01); // turn display on
+}
+
+void render(uint8_t *buf, struct render_area *area) {
+    // update a portion of the display with a render area
+    oled_send_cmd(OLED_SET_COL_ADDR); //0x21 Set column address
+    oled_send_cmd(area->start_col); //Start column
+    oled_send_cmd(area->end_col); //End column
+
+    oled_send_cmd(OLED_SET_PAGE_ADDR); //0x22 Set page address
+    oled_send_cmd(area->start_page); //Start page
+    oled_send_cmd(area->end_page); //End page
+
+    oled_send_buf(buf, area->buflen);
+}
+
 int bmp280_init() {
 	int retVal;
     // use the "handheld device dynamic" optimal setting (see datasheet)
@@ -346,6 +549,32 @@ void bmp280_get_calib_params(struct bmp280_calib_param* params) {
 
 #endif
 
+//Sends an entire line (page) to SSD1306 display (font fixed to 8x8)
+int ssd_1306_print(char *inString, int inPage) {
+    int i, numCol, charTablePtr;
+    char charToPrint;
+    struct render_area area = {start_col: 0, end_col : OLED_WIDTH - 1, start_page : inPage, end_page : inPage};
+
+    calc_render_area_buflen(&area);
+
+    memset(stringToWrite, 0x00, sizeof(stringToWrite)/sizeof(stringToWrite[0])); //Clean string filling with spaces
+    numCol = 0;
+
+    while( ( (charToPrint = *inString++) != '\0') & (numCol < OLED_WIDTH) ) { //Scan string
+//Calculate pointer to ASCII table, 32=SPACE is the first char, each char is represented by 8 bytes
+        if ( (charToPrint < ' ') | (charToPrint > '~') ) //Check if char is in the table
+            charToPrint = '#'; //Substitute char
+
+        charTablePtr = (charToPrint -32) << 3;
+        for (i=0; i<8; i++) //Move char representation (8 bytes long)
+            stringToWrite[numCol++] = char_set_8x8[charTablePtr++];        
+    }
+    
+    render(stringToWrite, &area);
+
+    return 0;
+}
+
 int SYSTEM_Init() {
 	int retVal;
 
@@ -375,20 +604,48 @@ int SYSTEM_Init() {
     gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
     gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
 
+   // run through the complete initialization process
+    oled_init();
+    // initialize render area for entire frame (128 pixels by 4 pages)
+    //struct render_area frame_area = {start_col: 0, end_col : OLED_WIDTH - 1, start_page : 0, end_page : OLED_NUM_PAGES -1};
+    struct render_area frame_area = {start_col: 0, end_col : OLED_WIDTH - 1, start_page : 0, end_page : OLED_NUM_PAGES - 1};
+    calc_render_area_buflen(&frame_area);
+
+    // zero the entire display
+    uint8_t buf[OLED_BUF_LEN];
+    fill(buf, 0x00);
+    render(buf, &frame_area);
+
+    // intro sequence: flash the screen 3 times
+    for (int i = 0; i < 3; i++) {
+        oled_send_cmd(0xA5); // ignore RAM, all pixels on
+        sleep_ms(500);
+        oled_send_cmd(0xA4); // go back to following RAM
+        sleep_ms(500);
+    }
+
+    // configure horizontal scrolling
+    oled_send_cmd(OLED_SET_HORIZ_SCROLL | 0x00);
+    oled_send_cmd(0x00); // dummy byte
+    oled_send_cmd(0x00); // start page 0
+    oled_send_cmd(0x00); // time interval
+    oled_send_cmd(0x03); // end page 3
+    oled_send_cmd(0x00); // dummy byte
+    oled_send_cmd(0xFF); // dummy byte
+
+    // let's goooo!
+    oled_send_cmd(OLED_SET_SCROLL | 0x01);
+
     // configure BMP280
 	if ( (retVal = bmp280_init()) != 0) {
 		gpio_put(LED_PIN, true);
 		printf("bmp280_init error. Returned value: %d.\n", retVal);
 		return retVal;
 		}
-	// retrieve fixed compensation params
-    struct bmp280_calib_param params;
-    bmp280_get_calib_params(&params);
 
 	printf("Init OK.\n");
 	return retVal;
 }
-
 
 bool R_TRIG(bool clk, bool *prValue) {
 	bool return_os;
@@ -399,7 +656,9 @@ bool R_TRIG(bool clk, bool *prValue) {
 
 int main() {
 	bool execute=false, ctr_pin, pr_Ctr=false, gp16_pin_os, gp16_pin_pr=false;
-	int status=IDLE, i, deltaCtr, retVal;
+    bool writeTemperatureToDisplay=false, writePressureToDsiplay=false;
+	char displayString[17]; //TODO use parameter
+    int status=IDLE, i, deltaCtr, retVal;
 	int32_t raw_temperature, raw_pressure, temperature, pressure;
 	long int	pulseCtr=0, oldPulseCtr=0;
 	struct funct_out retStruct;
@@ -411,18 +670,37 @@ int main() {
 
 	bmp280_get_calib_params(&params); //retrieve fixed compensation params. TBD: error handling
 
+    ssd_1306_print("Init ok...", 0);
+
 	while(true) {
 		SYSTEM_Clock();
 		
 		gp16_pin_os = R_TRIG(gpio_get(GP16_PIN), &gp16_pin_pr);
 		if (gp16_pin_os)
 			++pulseCtr;
+        
+        //To riduce cycle overload write to display at different times
+        if (writePressureToDsiplay) {
+            writePressureToDsiplay = false;
+            sprintf(displayString, "Press.=%.1f", pressure / 100.f);
+            ssd_1306_print(displayString, 1);
+            }
+
+        if (writeTemperatureToDisplay) {
+            writeTemperatureToDisplay = false;
+            writePressureToDsiplay = true;
+            sprintf(displayString, "Temp.=%.1f C", temperature / 100.f);
+            ssd_1306_print(displayString, 0);
+            }
+            
 
 		if (g_systemTimers.g_os_1m) {
 			bmp280_read_raw(&raw_temperature, &raw_pressure);
         	temperature = bmp280_convert_temp(raw_temperature, &params);
         	pressure = bmp280_convert_pressure(raw_pressure, raw_temperature, &params);
         	printf("Pressure = %.3f kPa\tTemp. = %.2f C\n", pressure / 100.f, temperature / 100.f); //Debug
+
+            writeTemperatureToDisplay = true;
 
 			deltaCtr = pulseCtr - oldPulseCtr;
 			execute = true;
